c1ce399c7dcb571f4a161618b26c2131689de08a
Pbone3 <pbone785@gmail.com>
2021-05-03 21:48:09 -0500

    Added missing localization, started infinite ammo

---
+++ Items/Ammo/Bullets/EndlessBulletItems.cs
@@ -0,0 +1,39 @@
+﻿using Terraria.ID;
+
+namespace PboneUtils.Items.Ammo.Bullets
+{
+    public class EndlessMeteorShot : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.MeteorShot;
+    }
+
+    public class EndlessSilverBullet : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.SilverBullet;
+    }
+
+    public class EndlessCrystalBullet : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.CrystalBullet;
+    }
+
+    public class EndlessCursedBullet : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.CursedBullet;
+    }
+
+    public class EndlessChlorophyteBullet : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.ChlorophyteBullet;
+    }
+
+    public class EndlessHighVelocityBullet : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.HighVelocityBullet;
+    }
+
+    public class EndlessIchorBullet : EndlessAmmoItem
+    {
+        public override int BaseAmmo => ItemID.IchorBullet;
+    }
+}
