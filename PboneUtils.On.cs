facdb9c2693eff52aabfb2ae6da4e3822fdb5e73
Stevie <xxlennygamerxx@gmail.com>
2021-06-11 02:12:22 -0700

    de-instance detour methods

--- PboneUtils.On.cs
+++ PboneUtils.On.cs
@@ -4,19 +4,21 @@ namespace PboneUtils
 {
     public partial class PboneUtils : Mod
     {
-        public void Load_On()
+        public static void Load_On()
         {
             On.Terraria.Player.QuickSpawnItem_int_int += Player_QuickSpawnItem_int_int;
             On.Terraria.Player.DropTombstone += Player_DropTombstone;
         }
 
-        private void Player_DropTombstone(On.Terraria.Player.orig_DropTombstone orig, Terraria.Player self, int coinsOwned, Terraria.Localization.NetworkText deathText, int hitDirection)
+        private static void Player_DropTombstone(On.Terraria.Player.orig_DropTombstone orig, Terraria.Player self,
+            int coinsOwned, Terraria.Localization.NetworkText deathText, int hitDirection)
         {
             if (!PboneUtilsConfig.Instance.NoMoreGraves)
                 orig(self, coinsOwned, deathText, hitDirection);
         }
 
-        private void Player_QuickSpawnItem_int_int(On.Terraria.Player.orig_QuickSpawnItem_int_int orig, Terraria.Player self, int item, int stack)
+        private static void Player_QuickSpawnItem_int_int(On.Terraria.Player.orig_QuickSpawnItem_int_int orig,
+            Terraria.Player self, int item, int stack)
         {
             if (!TreasureBagValueCalculator.Loading)
                 orig(self, item, stack);
