53cf2c5b034fca1cbdc2a59157977b66ee106b80
Pbone3 <pbone785@gmail.com>
2021-07-31 18:54:50 -0500

    Census compat is now l10n ready

--- PboneUtils/CrossMod/Ref/CensusCompatability.cs
+++ PboneUtils/CrossMod/Ref/CensusCompatability.cs
@@ -1,5 +1,6 @@
 ﻿using PboneLib.Core.CrossMod.Ref;
 using PboneUtils.NPCs.Town;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace PboneUtils.CrossMod.Ref.Content
@@ -16,10 +17,10 @@ namespace PboneUtils.CrossMod.Ref.Content
             // string:"TownNPCCondition" - int:npcid - string:condition
 
             if (PboneUtilsConfig.Instance.Miner)
-                GetMod().Call("TownNPCCondition", ModContent.NPCType<Miner>(), "Defeat Eater of Worlds/Brain of Cthulhu");
+                GetMod().Call("TownNPCCondition", ModContent.NPCType<Miner>(), Language.GetTextValue("Mods.PboneUtils.Census.Description.Miner"));
 
             if (PboneUtilsConfig.Instance.MysteriousTrader)
-                GetMod().Call("TownNPCCondition", ModContent.NPCType<MysteriousTrader>(), "Randomly arrives in the morning and leaves at night, after King Slime or Eye of Cthulhu");
+                GetMod().Call("TownNPCCondition", ModContent.NPCType<MysteriousTrader>(), Language.GetTextValue("Mods.PboneUtils.Census.Description.MysteriousTrader"));
         }
     }
 }
